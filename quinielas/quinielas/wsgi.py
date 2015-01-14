ab726b618b13ae71a26fdf9c02ad27a089d4f262
Daniel Arteaga <dani8art.da@gmail.com>
2015-01-14 02:11:04 +0100

    No cambiar path

--- quinielas/quinielas/wsgi.py
+++ quinielas/quinielas/wsgi.py
@@ -8,9 +8,6 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quinielas.settings")
 
 from django.core.wsgi import get_wsgi_application
