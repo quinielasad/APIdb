edc3983ed8b146bc810af6e132cb9f7f46741a3e
Daniel Arteaga <dani8art.da@gmail.com>
2015-01-14 21:48:09 +0100

    pruebas

--- quinielas/quinielas/wsgi.py
+++ quinielas/quinielas/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__))+"../")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quinielas.settings")
 
 from django.core.wsgi import get_wsgi_application
