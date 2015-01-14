aea3fb7ddcdec70e41d9674f23568e0faad57cb6
Daniel Arteaga <dani8art.da@gmail.com>
2015-01-14 21:52:57 +0100

    mas pruebas

--- quinielas/quinielas/wsgi.py
+++ quinielas/quinielas/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__))+"../")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quinielas.settings")
 
