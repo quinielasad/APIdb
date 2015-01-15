5cfc5cd7e50e51707f87cad35f2ee32b5a132c63
Daniel Arteaga <dani8art.da@gmail.com>
2015-01-15 16:51:50 +0100

    sdaf

--- api/v1/loginUser.php
+++ api/v1/loginUser.php
@@ -2,9 +2,9 @@
 session_start();
 
 //funcion para realizar login desde appMovil
-$data = json_decode($_GET["data"]);
+$data = json_decode($_GET["data"],true);
 
 
 
-echo($data.username);
+echo($data["username"]);
 ?>
